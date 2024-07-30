c8678e3cd724af4d52397b83a9f2932c707795cf
GarifullinII <ildaridrisovich@gmail.com>
2024-07-30 15:34:38 +0400

    Initial Commit

---
+++ FoodDiary/FoodDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodDiaryApp.swift
+//  FoodDiary
+//
+//  Created by Ildar Garifullin on 30/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct FoodDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
