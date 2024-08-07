8f687f4c207d882422b1e08f54fa76dc8622dd3c
GarifullinII <ildaridrisovich@gmail.com>
2024-08-07 17:12:14 +0400

    add UI

---
+++ FoodDiary/RoundedCorner.swift
@@ -0,0 +1,18 @@
+//
+//  RoundedCorner.swift
+//  FoodDiary
+//
+//  Created by Ildar Garifullin on 07/08/2024.
+//
+
+import SwiftUI
+
+struct RoundedCorner: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RoundedCorner()
+}
